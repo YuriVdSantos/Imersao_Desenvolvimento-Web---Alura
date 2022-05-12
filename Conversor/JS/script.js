c33e83d0fe491e4b9b7632a3d004aabc8c55dacb
Santos <yurivdsantos@outlook.com>
2022-05-12 12:38:30 -0300

    Atualizando Conversor de Moedas

--- Conversor/JS/script.js
+++ Conversor/JS/script.js
@@ -1,12 +1,34 @@
 function Converter() {
-  var valorInserido = document.getElementById("valor");
-  var apenasValor = valorInserido.value;
-  var valorNumerico = parseFloat(apenasValor);
+  var valorInserido = document.getElementById('valor')
+  var apenasValor = valorInserido.value
+  var valorNumerico = parseFloat(apenasValor)
 
-  var valorEmReal = apenasValor * 5;
-  console.log(valorEmReal);
+  var valorEmReal = valorNumerico * 5.18
+  valorArredondadoReal = valorEmReal.toFixed(2)
+  console.log(valorArredondadoReal)
 
-  var elementoValorConvertido = document.getElementById("valorConvertido");
-  var valorConvertido = "O valor em reais é R$ " + valorEmReal;
-  elementoValorConvertido.innerHTML = valorConvertido;
+  var elementoValorConvertidoReais = document.getElementById(
+    'valorConvertidoReais'
+  )
+  var valorConvertidoReais = 'O valor em reais é R$ ' + valorArredondadoReal
+  elementoValorConvertidoReais.innerHTML = valorConvertidoReais
+
+  var valorEmEuro = valorNumerico * 0.96
+  valorArredondadoEuro = valorEmEuro.toFixed(2)
+
+  var elementoValorConvertidoEuro = document.getElementById(
+    'valorConvertidoEuro'
+  )
+  var valorConvertidoEuro = 'O valor em Euro é R$ ' + valorArredondadoEuro
+  elementoValorConvertidoEuro.innerHTML = valorConvertidoEuro
+
+  var valorEmBitCoin = valorNumerico * 0.000034
+  valorArredondadoBitCoin = valorEmBitCoin.toFixed(5)
+
+  var elementoValorConvertidoBitCoin = document.getElementById(
+    'valorConvertidoBitCoin'
+  )
+  var valorConvertidoBitCoin =
+    'O valor em BitCoin é R$ ' + valorArredondadoBitCoin
+  elementoValorConvertidoBitCoin.innerHTML = valorConvertidoBitCoin
 }
